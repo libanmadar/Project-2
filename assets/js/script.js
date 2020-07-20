e19aa870f95103aec25e8e49a8381832c6a75fd6
unknown <liiban333@gmail.com>
2020-07-19 19:09:25 -0500

    Changed google map location

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 
 function initMap() {
-  // The location of Uluru
-  var uluru = {lat: -25.344, lng: 131.036};
-  // The map, centered at Uluru
+  // The location of minnetonka
+  var minnetonka = {lat: 44.92761074, lng: -93.59409689 };
+  // The map, centered at minnetonka
   var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
+      document.getElementById('map'), {zoom: 10, center: minnetonka});
   // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
+  var marker = new google.maps.Marker({position: minnetonka, map: map});
 }
